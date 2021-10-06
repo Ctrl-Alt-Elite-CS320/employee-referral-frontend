5bed88efc9d806958f86b0e43a6496229ce62211
Sebastian Monzon <smonzon360@gmail.com>
2021-10-05 20:49:24 -0400

    new login page with fancy animations

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './styles/login.css';
+import './js/login.js';
 /*
 Home Page
  */
@@ -8,26 +9,67 @@ Home Page
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is the homepage for our application
-        </p>
+      <header class="App-header">
         
+        Sinecure
       </header>
-      <div>
-        <form>
-          <input type="text" className="" placeholder="Username"></input>
-          <br></br>
-          <input type="text" className="" placeholder="Password"></input>
-          <br></br>
-          <button type="submit" className="">Login</button>
-          <p>or</p>
-          <button type="submit" className="">Sign Up</button>
-        </form>
+      <div class="materialContainer">
+        <div class="box">
+
+          <div class="title">LOGIN</div>
+
+          <div class="input">
+            <label for="name">Username</label>
+            <input type="text" name="name" id="name"></input>
+            <span class="spin"></span>
+          </div>
+
+          <div class="input">
+            <label for="pass">Password</label>
+            <input type="password" name="pass" id="pass"></input>
+            <span class="spin"></span>
+          </div>
+
+          <div class="button login">
+            <button><span>GO</span> <i class="fa fa-check"></i></button>
+          </div>
+
+          <a href="" class="pass-forgot">Forgot your password?</a>
+
+        </div>
+
+        <div class="overbox">
+          <div class="material-button alt-2"><span class="shape"></span></div>
+
+          <div class="title">REGISTER</div>
+
+          <div class="input">
+            <label for="regname">Username</label>
+            <input type="text" name="regname" id="regname"></input>
+            <span class="spin"></span>
+          </div>
+
+          <div class="input">
+            <label for="regpass">Password</label>
+            <input type="password" name="regpass" id="regpass"></input>
+            <span class="spin"></span>
+          </div>
+
+          <div class="input">
+            <label for="reregpass">Repeat Password</label>
+            <input type="password" name="reregpass" id="reregpass"></input>
+            <span class="spin"></span>
+          </div>
+
+          <div class="button">
+            <button><span>NEXT</span></button>
+          </div>
+
+        </div>
+      </div>
       </div>
       
-    </div>
+      
   );
 }
 
