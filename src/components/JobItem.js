5d53981806fd47d5a4749ada8f9d39540e7ee13c
Shubh <shubhv2@gmail.com>
2021-11-29 19:05:23 -0500

    yo

--- src/components/JobItem.js
+++ src/components/JobItem.js
@@ -1,40 +1,30 @@
 import React from "react";
 import "../styles/JobItem.css";
 
-
 class JobItem extends React.Component {
   render() {
+    let { name, EarlySalary, Description } = this.props;
+
     return (
       <div className="job-item">
-        <h2>Job Title</h2><br />
-        <p>Lorem ipsum dolor sit amet consectetur adip
-          Lorem ipsum dolor sit amet consectetur adip
-          Lorem ipsum dolor sit amet consectetur adip
-          Lorem ipsum dolor sit amet consectetur adip
-          Lorem ipsum dolor sit amet consectetur adip
-        </p>
+        <h2>{name}</h2>
+        <br />
+        <p>{Description}</p>
         <br></br>
-        <div className="job-tags"><a>#Software-developer</a> <a>#Skilled</a></div>
+        <div className="job-tags">
+          <a>#Software-developer</a> <a>#Skilled</a>
+        </div>
         <br></br>
         <div className="job-posted-details">
           <div className="profile-button">
             <div className="profile-photo">
-
               <img></img>
-
             </div>
-            &nbsp;
-            John Doe
-
+            &nbsp; John Doe
           </div>
 
-
-          <div className="owner-info">
-
-            Posted on 9/19/2021
-          </div>
+          <div className="owner-info">Posted on 9/19/2021</div>
         </div>
-
       </div>
     );
   }
--- src/views/JobFeedPage.js
+++ src/views/JobFeedPage.js
@@ -1,11 +1,29 @@
-import '../styles/Feed.css';
-import JobItem from '../components/JobItem.js';
-import Logo from '../components/Logo.js';
-import Filters from '../components/Filters';
-import ReferCandidate from '../components/ReferCandidate';
-import ProfileIcon from '../components/ProfileIcon';
+import "../styles/Feed.css";
+import React, { useState, useEffect } from "react";
+import JobItem from "../components/JobItem.js";
+import Logo from "../components/Logo.js";
+import Filters from "../components/Filters";
+import ReferCandidate from "../components/ReferCandidate";
+import ProfileIcon from "../components/ProfileIcon";
+import axios from "axios";
 
 function JobFeedPage() {
+  const [c, setC] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const { data: yo } = await axios.get("http://localhost:4000/jobs/");
+        return yo;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    // Update the document title using the browser API
+    fetchData().then((val) => {
+      setC(val);
+    });
+    window.scrollTo(0, 0);
+  });
   return (
     <div className="JobFeedPage">
       <div className="filter-container">
@@ -22,13 +40,15 @@ function JobFeedPage() {
             <ProfileIcon />
           </div>
         </div>
-        <div className="divider">
-
-        </div>
+        <div className="divider"></div>
         <div className="feed-container">
           <div className="jobs-list col-50">
             <div>
-              <input className="search" type="search" placeholder="Search listings" />
+              <input
+                className="search"
+                type="search"
+                placeholder="Search listings"
+              />
               <form>
                 <select className="list-sort">
                   <option value="all">All Listings</option>
@@ -37,33 +57,35 @@ function JobFeedPage() {
                 </select>
               </form>
             </div>
-            <br /><br />
+            <br />
+            <br />
 
             <div className="scroll-gradient"></div>
-
+            {c.map((elem) => {
+              return (
+                <JobItem name={elem.name} Description={elem.Description} />
+              );
+            })}
+            {/* <JobItem />
             <JobItem />
             <JobItem />
             <JobItem />
-            <JobItem />
-            <JobItem />
+            <JobItem /> */}
             <br></br>
             <br></br>
             <br></br>
             <br></br>
             <br></br>
             <br></br>
-
           </div>
           <div className="job-details-container col-50">
             <ReferCandidate />
           </div>
-
         </div>
         <div className="add-button">
           <h1>+</h1>
         </div>
       </div>
-
     </div>
   );
 }
