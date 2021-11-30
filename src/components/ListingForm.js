fd6de8f8fd0a517c52b1c38515e6772b31a664d3
Shubh <shubhv2@gmail.com>
2021-11-29 21:10:05 -0500

    added functionality for input

--- src/components/ListingForm.js
+++ src/components/ListingForm.js
@@ -1,43 +1,109 @@
 import React from "react";
-import '../styles/ListingForm.css';
+import "../styles/ListingForm.css";
+const axios = require("axios");
 
 class ListingForm extends React.Component {
-    render() {
-        return (
-            <div className="listing">
-                <form action="/action_page.php" target="_blank">
-                    <input className="job-text" type="text" placeholder="New Job Listing e.g. Software Developer" required name="listing" /><br/>
-                    <div className="textbox-container">
-                        <div className="col-50">
-                            <label for="salary">Yearly Salary:</label>
-                            <input className="entry-info" id="salary" type="text" placeholder="" required name="salary" />
-                        </div>
-                        <div className="col-50">
-                            <label for="exp">Minimum years of Exp:</label>
-                            <input className="entry-info" id="exp" type="text" placeholder="" required name="exp" />
-                        </div>
-                    </div>
-                    <div className="slider-container">
-                        <div className="col-50">
-                            <input className="slider" type="range" min="0" max="50" value="25" id="SalarySlider" />
-                        </div>
-                        <div className="col-50">
-                            <input className="slider" type="range" min="0" max="50" value="25" id="ExpSlider" />
-                        </div>
-                    </div>
-                    <textarea
-                        className="paragraph-text"
-                        placeholder="Job Description"
-                        cols="30"
-                        rows="10"
-                        maxLength="500"
-                    />
-                    <br /><br />
-                    <input className="post-button" type="submit" value="Post" />
-                </form>
+  state = { name: "", yearSalary: 0, minExp: 0, description: "" };
+  handleChangename = (event) => {
+    this.setState({ name: event.target.value });
+  };
+  handleChangeYearlySalary = (event) => {
+    this.setState({ yearSalary: event.target.value });
+  };
+  handleMinExp = (event) => {
+    this.setState({ minExp: event.target.value });
+  };
+  handleDesc = (event) => {
+    this.setState({ description: event.target.value });
+    // console.log(this.state);
+  };
+  handleSubmit = () => {
+    axios.post("localhost", this.state).then(function (response) {
+      console.log(response);
+    });
+  };
+
+  render() {
+    return (
+      <div className="listing">
+        <form action="/action_page.php" target="_blank">
+          <input
+            className="job-text"
+            type="text"
+            placeholder="New Job Listing e.g. Software Developer"
+            required
+            name="listing"
+            onChange={this.handleChangename}
+          />
+          <br />
+          <div className="textbox-container">
+            <div className="col-50">
+              <label for="salary">Yearly Salary:</label>
+              <input
+                className="entry-info"
+                id="salary"
+                type="text"
+                placeholder=""
+                required
+                name="salary"
+                onChange={this.handleChangeYearlySalary}
+              />
             </div>
-        );
-    }
+            <div className="col-50">
+              <label for="exp">Minimum years of Exp:</label>
+              <input
+                className="entry-info"
+                id="exp"
+                type="text"
+                placeholder=""
+                required
+                name="exp"
+                onChange={this.handleMinExp}
+              />
+            </div>
+          </div>
+          <div className="slider-container">
+            <div className="col-50">
+              <input
+                className="slider"
+                type="range"
+                min="0"
+                max="50"
+                value="25"
+                id="SalarySlider"
+              />
+            </div>
+            <div className="col-50">
+              <input
+                className="slider"
+                type="range"
+                min="0"
+                max="50"
+                value="25"
+                id="ExpSlider"
+              />
+            </div>
+          </div>
+          <textarea
+            className="paragraph-text"
+            placeholder="Job Description"
+            cols="30"
+            rows="10"
+            maxLength="500"
+            onChange={this.handleDesc}
+          />
+          <br />
+          <br />
+          <input
+            className="post-button"
+            type="submit"
+            value="Post"
+            onClick={() => this.handleSubmit()}
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
-export default ListingForm
+export default ListingForm;
--- src/views/NewPosition.js
+++ src/views/NewPosition.js
@@ -1,29 +1,29 @@
-import '../styles/NewPosition.css';
-import Logo from '../components/Logo.js';
-import ProfileIcon from '../components/ProfileIcon';
-import ListingForm from '../components/ListingForm';
+import "../styles/NewPosition.css";
+import Logo from "../components/Logo.js";
+import ProfileIcon from "../components/ProfileIcon";
+import ListingForm from "../components/ListingForm";
 
 function NewPosition() {
   return (
     <div className="NewPosition">
-        <div className="logo-alignment">
-            <Logo />
+      <div className="logo-alignment">
+        <Logo />
+      </div>
+      <div className="main-content">
+        <div className="position-header">
+          <div className="col-80">
+            <h1>New Listing</h1>
+            <h3>Manager</h3>
+          </div>
+          <div className="col-20">
+            <ProfileIcon />
+          </div>
         </div>
-        <div className="main-content">
-            <div className="position-header">
-                <div className="col-80">
-                    <h1>New Listing</h1>
-                    <h3>Manager</h3>
-                </div>
-                <div className="col-20">
-                    <ProfileIcon />
-                </div>
-            </div>
-            <div className="divider"></div>
-            <div className="listing-form">
-                <ListingForm />
-            </div>
+        <div className="divider"></div>
+        <div className="listing-form">
+          <ListingForm />
         </div>
+      </div>
     </div>
   );
 }
